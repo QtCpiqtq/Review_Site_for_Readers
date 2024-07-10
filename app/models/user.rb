b784efbf205f91537abfd1fdc8108c8f3c0aec54
Hidetoshi tsutsumi <tsutsutsutsumi223@gmail.com>
2024-07-10 15:04:56 +0000

    [add]レビュー投稿を機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_one_attached :image
-  has_many :faborite_books, dependent: :destroy
+  has_many :favorite_books, dependent: :destroy
+  has_many :books, through: :favorite_books
+  has_many :reviews, dependent: :destroy
   
   def get_image(width, height)
     image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def added_to_favorite?(book_id)
+    self.favorite_books.pluck(:book_id).include?(book_id)
   end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,23 +11,24 @@ Rails.application.routes.draw do
     get 'wish_lists/index'
   
     get 'followers/index'
-  
-    get 'reviews/show'
-    get 'reviews/new'
     
-    resources :favorite_books, only: [:create]
+    resources :favorite_books, only: [:create, :destroy]
     
+    resources :books, only: [:index]
     get 'books/search' => 'books#search'
-    resources :books, only: [:index, :show]
+    get 'books/:isbn', to: "books#show", as: "book"
+    resources :reviews, only: [:show]
+    get 'books/:isbn/reviews/new', to: "reviews#new", as: "new_book_review"
+    post 'books/:isbn/reviews', to: "reviews#create", as: "book_reviews"
     
-    get 'mypage' => 'users#mypage'
+    
+    get 'mypage/:id' => 'users#mypage', as: 'mypage'
     get 'information' => 'users#information'
     get 'information/edit' => 'users#edit'
     patch 'information/edit' => 'users#update'
     get 'unsubscribe' => 'users#unsubscribe'
     patch 'withdraw' => 'users#withdraw'
     delete 'image_destroy' => 'users#destroy'
-    get 'userpage' => 'users#show'
   end
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
